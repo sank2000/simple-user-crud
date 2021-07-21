f13e5b7c322d6278c01f0a0de9eb39442b79bd36
sank2000 <santhoshvelr@yahoo.com>
2021-07-21 15:00:22 +0530

    add user functionality added ✔

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -2,4 +2,5 @@ export { default as AppBar } from './AppBarCustom';
 export { default as Alert } from './Alert';
 export { default as NotFound } from './NotFound';
 export { default as Dialog } from './Dialog';
+export { default as ButtonLoader } from './ButtonLoader';
 export * from './Table';
--- frontend/src/pages/Add.jsx
+++ frontend/src/pages/Add.jsx
@@ -1,7 +1,125 @@
+import { useState } from 'react';
+import {
+  TextField,
+  Button,
+  Container,
+  Typography,
+  Snackbar,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+
+import { Alert, ButtonLoader } from '../components';
+
+const initialValue = {
+  name: '',
+  phoneNumber: '',
+  email: '',
+};
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
 export default function Add() {
+  const classes = useStyles();
+  const [data, setData] = useState(initialValue);
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
+  const [snack, setSnack] = useState({
+    open: false,
+    type: 'error',
+    message: 'hey ',
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (loading) return;
+    setLoading(true);
+    try {
+      const res = await axios.post('/user', data);
+      setLoading(false);
+      setSnack({
+        open: true,
+        type: 'success',
+        message: 'User created successfully',
+      });
+      setTimeout(() => {
+        history.push('/');
+      }, 2000);
+    } catch (error) {
+      const statusCode = error.response ? error.response.status : null;
+      if (statusCode === 400) {
+        setLoading(false);
+        setSnack({
+          open: true,
+          type: 'error',
+          message: error.response.data.error ?? 'Something went wrong !',
+        });
+        console.log('profile not found');
+      }
+    }
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setSnack((old) => ({ ...old, open: false }));
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData((old) => ({
+      ...old,
+      [name]: value,
+    }));
+  };
+
   return (
-    <>
-      <h1>add</h1>
-    </>
+    <Container maxWidth="sm" style={{ marginTop: '30px' }}>
+      <Typography variant="h5">Create User</Typography>
+      <form className={classes.root} onSubmit={handleSubmit}>
+        <TextField
+          label="Name"
+          required
+          fullWidth
+          name="name"
+          value={data.name}
+          onChange={handleChange}
+        />
+        <TextField
+          label="Phone Number"
+          type="number"
+          required
+          fullWidth
+          name="phoneNumber"
+          value={data.phoneNumber}
+          onChange={handleChange}
+        />
+        <TextField
+          label="Email"
+          required
+          type="email"
+          name="email"
+          fullWidth
+          value={data.email}
+          onChange={handleChange}
+        />
+        <Button variant="contained" color="primary" type="submit">
+          Save {loading && <ButtonLoader />}
+        </Button>
+      </form>
+      <Snackbar open={snack.open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity={snack.type}>
+          {snack.message}
+        </Alert>
+      </Snackbar>
+    </Container>
   );
 }
